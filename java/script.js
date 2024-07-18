7ecba44d2822f5677b4a6bd8e6c61bb2d8de6938
RubesTeamWeb <mrmohamedepn2006@gmail.com>
2024-07-19 01:43:09 +0200

    edit before

--- java/script.js
+++ java/script.js
@@ -14,3 +14,21 @@ document.addEventListener('DOMContentLoaded', function () {
         slides[currentSlide].classList.add('active');
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let slides = document.querySelectorAll('.slide1');
+    let currentSlide = 0;
+
+    document.querySelector('.next1').addEventListener('click', function () {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+    });
+
+    document.querySelector('.prev1').addEventListener('click', function () {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+        slides[currentSlide].classList.add('active');
+    });
+});
